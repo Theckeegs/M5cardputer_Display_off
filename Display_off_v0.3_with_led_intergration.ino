3ebd72929d6d30ecb7f4d7cb2fdb8bc81e30beb0
Theckeegs <theckeegs@gmail.com>
2024-09-03 21:32:36 +0100

    Update Display_off_v0.3_with_led_intergration.ino

--- Display_off_v0.3_with_led_intergration.ino
+++ Display_off_v0.3_with_led_intergration.ino
@@ -23,6 +23,9 @@
 
 Adafruit_NeoPixel pixels(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
+float lastVoltage = 0.0;
+bool ledOn = false;
+
 void setup() {
     auto cfg = M5.config();
     M5Cardputer.begin(cfg);
@@ -30,37 +33,57 @@ void setup() {
     M5.Lcd.setTextSize(2.5);
     M5.Lcd.fillScreen(0x0000);
     M5.Lcd.setCursor(0, 1);
-    M5.Lcd.println("Display_off_v0.4");
+    M5.Lcd.println("Display_off_v0.5");
     M5.Lcd.setCursor(0, 30, 1);
     M5.Lcd.println("By Kestrelrain");
     M5.Lcd.setCursor(0, 60, 1);
     M5.Lcd.println("LED integration by theckeegs");
     delay(4000);
     
+    M5.Lcd.fillScreen(0x0000);
+    M5.Lcd.setCursor(0, 30, 1);
+    M5.Lcd.println("Please unplug Cardputer");
+    delay(5000);
+    
     M5Cardputer.Display.setBrightness(0);
     M5Cardputer.Display.clear();
     
     pixels.begin();
     pixels.setBrightness(50);  // Set LED brightness (0-255)
+    
+    lastVoltage = M5.Power.getBatteryVoltage();
 }
 
 void loop() {
     M5.update();
     float batteryVoltage = M5.Power.getBatteryVoltage();
-    bool isCharging = batteryVoltage > 3800.0;
     int batteryLevel = M5.Power.getBatteryLevel();
-
-    if (!isCharging) {
-        // Not charging, turn off LED
-        pixels.setPixelColor(0, pixels.Color(0, 0, 0));
-    } else if (isCharging && batteryLevel < 100) {
-        // Charging, turn LED red
-        pixels.setPixelColor(0, pixels.Color(255, 0, 0));
-    } else if (isCharging && batteryLevel >= 100) {
-        // Fully charged, turn LED green
-        pixels.setPixelColor(0, pixels.Color(0, 255, 0));
+    
+    // Check for voltage change
+    if (batteryVoltage - lastVoltage > 50.00) {
+        ledOn = true;
+    } else if (lastVoltage - batteryVoltage > 50.00) {
+        ledOn = false;
     }
-
+    
+    lastVoltage = batteryVoltage;
+    
+    if (ledOn) {
+        if (batteryLevel == 100) {
+            // Fully charged, turn LED green
+            pixels.setPixelColor(0, pixels.Color(0, 255, 0));
+        } else if (batteryLevel > 90) {
+            // Above 90%, turn LED orange
+            pixels.setPixelColor(0, pixels.Color(255, 165, 0));
+        } else {
+            // Below 90%, turn LED red
+            pixels.setPixelColor(0, pixels.Color(255, 0, 0));
+        }
+    } else {
+        // LED off
+        pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+    }
+    
     pixels.show();
     delay(1000);  // Update every second
 }
